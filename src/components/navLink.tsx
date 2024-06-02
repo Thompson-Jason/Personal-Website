7a8c3db7cd2f56feb12a5e52fdb255214a441e34
Jason Thompson <jason@jasonthompson.org>
2024-06-02 17:05:02 -0400

    Create new navLink component to add extra css to the link.

---
+++ src/components/navLink.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from 'react';
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+
+// @ts-ignore
+const NavLink = ({ link }) => {
+
+    const pathName: string = usePathname();
+
+    return (
+        <div>
+            <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.url}>{link.title}</Link>
+        </div>
+    );
+};
+
+export default NavLink;
