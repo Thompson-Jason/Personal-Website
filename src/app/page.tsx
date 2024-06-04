ff104221973058ff1f6c1d6848ad420051a7660d
Jason Thompson <jason@jasonthompson.org>
2024-06-04 17:56:09 -0400

    Replacing placeholder lorem ipsum text with a professional summary on the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Homepage = () => {
             </div>
             <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
                 <h1 className="text-4xl font-bold md:text-6xl">Looking for Software Engineer roles.</h1>
-                <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="md:text-xl">Results-oriented Computer Science graduate with strong skills in Java, C++, and Python. Experienced Software Engineer with expertise in Interactive Voice Response (IVR) systems, utilizing industry-standard tools and Agile methodologies. Adept at problem-solving and collaborative team efforts.</p>
                 <div className="flex w-full gap-4">
                     <button className=" p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
                     <button className="p-4 rounded-lg ring-1 ring-black">Contact Me</button>
