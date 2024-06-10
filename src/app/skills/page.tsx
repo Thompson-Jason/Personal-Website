2ef161e0aeb05bcc4187e35019f28bb17b8d1786
Jason Thompson <jason@jasonthompson.org>
2024-06-10 14:32:25 -0400

    Moving skills type and skillsList to its own data file

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,10 +2,9 @@
 import Skillblock from "@/components/skillblock";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { Skill, skillList } from "@/data/skills";
 
 const SkillsPage = () => {
-  type Skill = { name: string; desc: string };
-
   const [isRendered, setIsRendered] = useState(false);
 
   useEffect(() => {
@@ -15,105 +14,6 @@ const SkillsPage = () => {
 
     return () => clearTimeout(timer);
   }, []);
-
-  const skillList: Array<Skill> = [
-    {
-      name: "Java",
-      desc: "I started learning Java in my last year at college. C++ was the main language we used so I would go back and re-do / re-figureout my projects in Java. This gave me a very good base \
-      understanding of the language. I continued to expand my knowledge of Java using it for two years at my previous role at [24]7.ai it was during this time that I showed my deep understanding \
-      of the language by obtaining a Oracle Certified Associate - Java SE 8 Programmer Certification in September of 2023.",
-    },
-    {
-      name: "C++",
-      desc: "C++ was the language of choice for my college, this led me to utilizing it in almost all of my classes for the four years I was there. Throughout those four years I continued \
-      to improve on not only my C++ skills but my overall programming skills that have helped me in any language I choose to write in. While I have not yet used C++ professionally I \
-      believe that I will still be able utilize this language at a high level of skill",
-    },
-    {
-      name: "JavaScript",
-      desc: "JavaScript is something that I used to mess around with for years making discord bots here and there but it wasn't until last year that I decided it was time to actually learn \
-       the language. I have been studing it for a while and I feel confident in my ability to perform well in this language.",
-    },
-    {
-      name: "TypeScript",
-      desc: "I have a strong foundation in JavaScript, having used it for a year, and I am currently in the process of transitioning to TypeScript. My journey with TypeScript has involved \
-      gradually incorporating its features into my projects, allowing me to enhance code quality and maintainability through static typing and advanced type definitions. This transition has \
-      improved my ability to catch errors early and develop more robust applications.",
-    },
-    {
-      name: "Python",
-      desc: "Python is another language that I started to learn in college. At the time I only really knew C++ and I wanted to expand my knowledge to other languages. It was because of \
-      this that I decided to take my Artificial Intelligence course using Python instead of C++ like most of the class was using",
-    },
-    {
-      name: "React.js",
-      desc: "I have been actively learning React.js and applying my knowledge to build a website, showcasing my growing proficiency with this powerful JavaScript library. Through hands-on \
-      experience, I have become familiar with React's core concepts such as components, state management, and hooks, enabling me to create dynamic and responsive user interfaces. This \
-      project reflects my ability to leverage React.js to develop modern web applications.",
-    },
-    {
-      name: "Next.js",
-      desc: "I have been actively learning Next.js and applying my knowledge to build a website, demonstrating my growing proficiency with this versatile React framework. Through practical \
-      experience, I have become familiar with Next.js features such as server-side rendering, static site generation, and API routes. This project showcases my ability to leverage Next.js \
-      for creating highly performant and SEO-friendly web applications.",
-    },
-    {
-      name: "TailwindCSS",
-      desc: "I have been actively learning TailwindCSS and applying my knowledge to build a website, highlighting my growing proficiency with this utility-first CSS framework. Through \
-      hands-on experience, I have become adept at using TailwindCSS to create responsive and visually appealing designs with minimal custom CSS. This project illustrates my ability to \
-      efficiently style web applications using TailwindCSS, enhancing both development speed and design consistency.",
-    },
-    {
-      name: "Jenkins",
-      desc: "This is a skill that I developed over the course of my tenure at [24]7.ai. Jenkins was the CI/CD tool of choice with my team. During my time there I used Jenkins almost everyday \
-      which most of the time would just be running already built Jenkinsfiles but would also envolve improving the workflow or writing whole new workflows for new projects from scratch.",
-    },
-    {
-      name: "Apache Maven",
-      desc: "I often use Maven to build any Java project I am working on. It was also the building tool used at my previous company [24]7.ai I have extensive knowledge in Maven in updating and \
-      creating POM files.",
-    },
-    {
-      name: "Git",
-      desc: "I have been using git with various projects both professionally and personally. I started using git in college which gives me seven years of experience using git.",
-    },
-    {
-      name: "Github",
-      desc: "I started using GitHub back in college for hosting my various projects. I have been exploring GitHub more and more as time goes on which led me to learning GitHub acitons and various CI/CD \
-      practices from there. I recently recieved a certification Career Essentials in GitHub Professional Certificate in May 2024",
-    },
-    {
-      name: "Jira",
-      desc: "I have a solid two-year background in using Jira, gained from my previous job where it was integral to our workflow. During this period, I consistently utilized Jira to track every story, \
-      ensuring all project tasks were meticulously documented and monitored.",
-    },
-    {
-      name: "Agile Development",
-      desc: "I have a robust understanding of Agile Development, which I first learned during my final year at college. Over the past two years at [24]7.ai, I applied Agile methodologies extensively, \
-      participating in sprint planning, daily stand-ups, and retrospectives. My experience includes collaborating with cross-functional teams to deliver iterative improvements, ensuring flexibility, \
-      and continuously aligning project outcomes with customer needs.",
-    },
-    {
-      name: "SQL",
-      desc: "I started learning SQL about halfway though my college carrer which gave me time to familarize myself with the language before utilizing it at my previous job with [24]7.ai in a \
-      mySQL environment. While I only have professional experience with mySQL I have a strong understanding of the fundementals and I am confident that I can utlize these skills in any SQL Database type",
-    },
-    {
-      name: "Apache Kafka",
-      desc: "I started using Kafka about two years ago when my company at the time needed it for a project. I spent a ton of time learning as much as I could about Kafka \
-      so I knew exactly how I wanted to build the project as the project was heavly dependent on Kafka and it was the first project I was taking from conception to produciton.",
-    },
-    {
-      name: "Spring Boot",
-      desc: "Spring Boot is a skill that I started learning a little over a year ago to expand my knowledge of Java. While I have not yet gotten the chance to use it professionally I \
-      really like the framework and believe that it can be used to improve a lot of codebases even if just on a boiler plate level. ",
-    },
-    {
-      name: "Multithreading",
-      desc: "Multithreading is a skill that I started learning in college which gave me a strong understanding of the fundamentals of multithreading and the specifics of multithreading in C++. \
-      I was able to successfully utilize my strong knowledge of multithreading to help in creating a complex multithreaded application with my previous role at [24]7.ai",
-    },
-  ];
 
   return (
     <div className={`overflow-x-hidden ${isRendered ? "" : "hidden"}`}>
