e9cdad982cff280d270aa2bb554ee42abf508ebd
Jason Thompson <jason@jasonthompson.org>
2024-06-10 14:35:30 -0400

    Create a WIP portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,8 @@
 "use client";
+import PortfolioPane from "@/components/portfoliopane";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { projects, Project } from "@/data/projects";
 
 const PortfolioPage = () => {
   const [isRendered, setIsRendered] = useState(false);
@@ -11,7 +14,24 @@ const PortfolioPage = () => {
 
     return () => clearTimeout(timer);
   }, []);
-  return <div className={isRendered ? "" : "hidden"}>PortfolioPage</div>;
+
+  return (
+    <div className={isRendered ? "" : "hidden"}>
+      <h1 className="text-4xl text-center font-semibold sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        My different projects
+      </h1>
+      <h3 className="text-xl text-center font-semibold">
+        Click on each project to learn more!
+      </h3>
+      <div className="flex flex-row flex-wrap justify-center gap-5 sm:py-3 md:py-8 lg:py-16 xl:py-24">
+        {projects.map((project: Project) => (
+          <Link href={`/portfolio/${project.name}`} key={project.name}>
+            <PortfolioPane project={project} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PortfolioPage;
