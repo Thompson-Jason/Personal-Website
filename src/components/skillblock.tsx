ccfce6d6515c8ea831a4a424a5e5e6634ccad372
Jason Thompson <jason@jasonthompson.org>
2024-06-10 10:00:24 -0400

    Update CSS to center the text in the skillblock

--- src/components/skillblock.tsx
+++ src/components/skillblock.tsx
@@ -7,12 +7,14 @@ const Skillblock = (props: PropsList) => {
   return (
     <div
       id={props.skill.name}
-      className="bg-red-100 outline-double rounded-3xl flex flex-col justify-center items-center mx-auto"
+      className="bg-red-100 outline-double rounded-3xl flex flex-col justify-center items-center mx-auto overflow-auto"
     >
       <h1 className="flex items-center justify-center font-semibold text-4xl mt-5">
         {props.skill.name}
       </h1>
-      <div className="flex justify-center my-5 px-5">{props.skill.desc}</div>
+      <div className="flex justify-center text-center my-5 px-5">
+        {props.skill.desc}
+      </div>
     </div>
   );
 };
