0beec65245443ec9ff4ef26508621ff4defcbd62
Jason Thompson <jason@jasonthompson.org>
2024-06-11 10:50:06 -0400

    Add coming soon project to express that I am always working on new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,4 +16,15 @@ export const projects: Project[] = [
         alt_text: "A picture of the homepage of my personal portfolio website",
         github_url: "https://github.com/Thompson-Jason/Personal-Website",
       },
+
+
+      //Leave this project as the last project
+      {
+        name: "Coming_Soon",
+        short_desc: "More projects are always being worked on",
+        long_desc: "I am always working on new projects and continuously enhancing my skills, which means my portfolio is constantly evolving. As I explore new technologies and ideas, I regularly add fresh content to my personal website. This dynamic approach ensures that the portfolio reflects my latest work and growth as a developer. Stay tuned for exciting updates and innovative projects that will showcase my ongoing commitment to learning and development.",
+        img: "/coming-soon.png",
+        alt_text: "A picture of the text coming soon",
+        github_url: "https://github.com/Thompson-Jason",
+      }
 ]
